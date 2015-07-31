4ecd0c1cf465a3118c547b7940f65e55b4d20b97
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-07-31 15:37:55 +0100

    adding method to creare array with random values and to find el index

--- ClassSolution.java
+++ ClassSolution.java
@@ -1,8 +1,16 @@
+import java.util.Random;
+import java.util.Arrays;
+
 public class Solution{
     
     public static void main(String[]args){
         
-        int[] arr = new int[7];
+        final int size = 15;
+        final int size1 = 7;
+        Solution sol = new Solution();
+        boolean found = true;
+        int index = 0;
+        int[] arr = new int[size1];
         arr[0] = -7;
         arr[1] = 1;
         arr[2] = 5;
@@ -10,17 +18,47 @@ public class Solution{
         arr[4] = -4;
         arr[5] = 3;
         arr[6] = 0;
+        int[] Ar = {-16440, 10064, -17842, -18461, 12960, 8609, -4534, 4804, 19642, -14403, 11741, -2818, 9002, -2324, 16313 };
         
         try {
-            Solution sol = new Solution();
-            System.out.println("Equilibrium solution: " + sol.solution(arr, -2));
+            
+            System.out.println("Equilibrium solution: " + sol.solution(arr, 3));
+            System.out.println("Equilibrium solution: " + sol.solution(Ar, 14));
+            int[] theArray;
+            System.out.println("\nHOLD ON, THIS MAY TAKE A WHILE TO PROCESS");
+             do{ 
+                    theArray = sol.createElements(size);
+                    for(int i = 0; i < theArray.length; i++){
+                        if(sol.solution(theArray, i) == 0) {
+                            found = false;
+                            index = i;
+                        }
+                    }
+             } while(found);
+          
+            System.out.println("Folloving array "+ Arrays.toString(theArray) + 
+                                " \nhas an equilibrium index in " + index + 
+                                ",\nin position " + (index + 1) + 
+                                ",\nhold value: "+ theArray[index] );
+        
         }
         catch (Exception e) {
             System.out.println(e.toString());
         }
-        
     }
    
+    public int[] createElements(int size){
+        Random rm = new Random();
+        int[] Ar = new int[size];
+        final int maximum = 10000;
+        final int minimum = -10000;
+        int n = maximum - minimum + 1;
+        
+        for(int i = 0; i < Ar.length; i++){
+            Ar[i] = rm.nextInt() % n;
+        }
+        return Ar;
+    }
     
     public int solution(int[] A, int N){
         
@@ -42,3 +80,5 @@ public class Solution{
        // System.out.println(totalSumRight + " :R and L: " + totalSumLeft);
         if(totalSumRight == totalSumLeft) return 0; else return -1;
     }
+
+}
